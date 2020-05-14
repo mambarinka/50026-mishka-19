f4c785eeaaf44c3b1482f24d24a8810eeb2d46c7
Анна <mambarinka@mail.ru>
2020-05-14 12:20:36 +0400

    минимизирует CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,11 @@ var sourcemap = require("gulp-sourcemaps");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+
+
+
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -16,6 +21,8 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
