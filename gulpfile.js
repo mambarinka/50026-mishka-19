f3f26bface4cba80fc75b885e6b66d37d71c6043
Анна <mambarinka@mail.ru>
2020-05-17 18:36:00 +0400

    ie and style.css

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,8 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
     .pipe(csso())
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
